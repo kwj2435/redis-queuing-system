1b94a9cb06947809ea0b3c89b98b32d87b791a67
kimuijin <kimuj0035@hanssem.com>
2023-11-16 14:13:35 +0900

    Initial commit

---
+++ src/main/java/com/system/queuing/RedisQueuingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.queuing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisQueuingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisQueuingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/system/queuing/RedisQueuingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.queuing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisQueuingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
